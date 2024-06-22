4062c47f81b37f19dd2e51a88d00d84342d72ebf
Himagiri <himagiri06@gmail.com>
2024-06-22 12:00:51 +0530

    Display current page number in pagination results and changed button text in prev and next buttons

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -15,23 +15,29 @@ class PaginationView extends ContainerView {
   _generateMarkup() {
     const currPage = this._data.page;
 
-    const numPages = Math.ceil(
-      this._data.recipes.length / this._data.resultsPerPage
-    );
+    const numPages = Math.ceil(this._data.recipes.length / this._data.resultsPerPage);
 
     //  Page 1 - more results available
-    if (currPage === 1 && numPages > 1) {
-      return this.#generateMarkupButton('next', currPage + 1);
-    }
+    if (currPage === 1 && numPages > 1)
+      return `
+        ${this.#generateMarkupButton('prev', currPage + 1, { disabled: true })}
+        ${this.#generatePageNumber(currPage)}
+        ${this.#generateMarkupButton('next', currPage + 1)}
+      `;
 
     // Last Page
     if (currPage === numPages && numPages > 1)
-      return this.#generateMarkupButton('prev', currPage - 1);
+      return `
+    ${this.#generateMarkupButton('prev', currPage - 1)}
+    ${this.#generatePageNumber(currPage)}
+    ${this.#generateMarkupButton('next', currPage - 1, { disabled: true })}
+    `;
 
     // Between first and last pages
     if (currPage > 1 && this._data.page < numPages)
       return `
         ${this.#generateMarkupButton('prev', currPage - 1)}
+        ${this.#generatePageNumber(currPage)}
         ${this.#generateMarkupButton('next', currPage + 1)}
       `;
 
@@ -39,26 +45,51 @@ class PaginationView extends ContainerView {
     return '';
   }
 
-  #generateMarkupButton(type = 'next', pageNum) {
+  #generateMarkupButton(type = 'next', pageNum, options = {}) {
+    const { disabled = false } = options;
     if (type === 'prev')
       return `
-      <button class="btn--inline pagination__btn--prev" data-goto="${pageNum}">
+      <button class="btn--inline pagination__btn--prev ${disabled ? 'btn--disabled' : ''}" data-goto="${pageNum}">
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-left"></use>
             </svg>
-            <span>Page ${pageNum}</span>
+            <span>Prev</span>
         </button>
     `;
 
     return `
-      <button class="btn--inline pagination__btn--next" data-goto="${pageNum}">
-            <span>Page ${pageNum}</span>
+      <button class="btn--inline pagination__btn--next ${disabled ? 'btn--disabled' : ''}" data-goto="${pageNum}">
+            <span>Next</span>
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-right"></use>
             </svg>
         </button>
     `;
   }
+
+  #generatePageNumber(pageNum) {
+    return `<span class="pagination__details">${pageNum}</span>`;
+  }
+  // #generateMarkupButton(type = 'next', pageNum) {
+  //   if (type === 'prev')
+  //     return `
+  //     <button class="btn--inline pagination__btn--prev" data-goto="${pageNum}">
+  //           <svg class="search__icon">
+  //             <use href="${icons}#icon-arrow-left"></use>
+  //           </svg>
+  //           <span>Page ${pageNum}</span>
+  //       </button>
+  //   `;
+
+  //   return `
+  //     <button class="btn--inline pagination__btn--next" data-goto="${pageNum}">
+  //           <span>Page ${pageNum}</span>
+  //           <svg class="search__icon">
+  //             <use href="${icons}#icon-arrow-right"></use>
+  //           </svg>
+  //       </button>
+  //   `;
+  // }
 }
 
 export default new PaginationView();
